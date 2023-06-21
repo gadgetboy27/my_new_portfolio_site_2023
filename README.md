a9b366b08b15dbad4467b38518448100b12b736a
Henry-Peti <iamgadgetboy@gmail.com>
2023-06-22 04:37:03 +1200

    trying colours

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-## Personal Portfolio for CoolDaddyDesign.com
+## Personal Portfolio for Iamgadgetboy.com
 ## Main design features and code from Adrian@Javascript Mastery
 ![Portfolio Website](https://i.ibb.co/WgPMpts/image.png)
 # my_new_portfolio_site_2023
+
+
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,7 +10,7 @@ const Header = () =>  (
     <Div1>
       <Link legacyBehavior href='/'>
         <a style={{ display: 'flex', alignItems: 'center', color: 'grey', size: '4rem'  }}>
-          <DiCssdeck size='4rem' color='purple'/> <Span> iamgadgetboy</Span>
+          <DiCssdeck size='4rem' color='purple'/> <Span> iamgadgetboy.com</Span>
         </a>
       </Link>
     </Div1>
