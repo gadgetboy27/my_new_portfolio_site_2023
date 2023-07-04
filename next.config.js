2cfd56570bf44f1c0502fd189dcf4201b50b9b61
Henry-Peti <iamgadgetboy@gmail.com>
2023-07-05 02:25:49 +1200

    trying tmage responsiveness

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: true,
       images: {
-        allowFutureImage: true,
+        unoptimized: true,
     },
   }
   
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -13,7 +13,7 @@ const Hero = (props) => (
       <SectionTitle main center>
         <Link legacyBehavior href='/'>
           <a style={{ display: 'flex', alignItems: 'left', color: 'grey', size: '3rem'  }}>
-          <Image src={ cooldaddy } size='100vw' alt='cooldaddy logo'/> 
+          <Image src={ cooldaddy } height='auto' width='100%' size='100vw' alt='cooldaddy logo'/> 
           </a>
         </Link>
       </SectionTitle>
